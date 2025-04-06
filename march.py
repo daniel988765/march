072a30f1f246d295fa2d6e29a0b607c8111738d5
daniel988765 <dbc.stick@gmail.com>
2025-04-06 12:56:30 +0200

    Create march.py

---
+++ march.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+import random
+
+pygame.init()
+
+iron_man =pygame.image.load("superhero/iron_man.jpeg")
+iron_man = pygame.image.scale(iron_man,(100,100))
+
+spideman=pygame.image.load("superhero/spideman.jpeg")
+spideman = pygame.image.scale(spideman,(100,100))
+
+black_widow = pygame.image.load("superhero/black_widow.jpeg")
+black_widow = pygame.image.scale(black_widow,(100,100))
+
+thor = pygame.image.load("superhero/thor.jpeg")
+thor = pygame.image.scale(thor,(100,100))
+
+
+
+
